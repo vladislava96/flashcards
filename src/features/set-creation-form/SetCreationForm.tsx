c619eb3c055f1c3fc105adfbd97a552faa05a186
Vladislava <vladislavaskrabova@gmail.com>
2024-10-30 22:20:10 +0300

    feat: hide set creation form

--- src/features/set-creation-form/SetCreationForm.tsx
+++ src/features/set-creation-form/SetCreationForm.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState } from 'react';
 import { useAppDispatch } from '../../app/hooks';
 import { addSet } from '../setsList/setsListSlice';
 import styles from './SetCreationForm.module.css';
+import { activeCreationForm } from './SetCreationFormSlice';
 
 export function SetCreationForm() {
   const [name, setName] = useState("");
@@ -13,16 +14,18 @@ export function SetCreationForm() {
       id: -1,
       name: name,
       cards: []
-    }))
+    }));
+    setName("");
+    dispatch(activeCreationForm(false))
   };
 
   return (
     <form className={styles.setCreationForm} onSubmit={onFormSubmit}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <input type="submit" value="Add"></input>
     </form>
   )
