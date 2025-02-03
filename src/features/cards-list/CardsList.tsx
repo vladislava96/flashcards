edde5966a7377ca14a960f8169f11fbcabdc45b2
Vladislava <vladislavaskrabova@gmail.com>
2025-02-03 12:10:14 +0300

    feat: delete creation button

--- src/features/cards-list/CardsList.tsx
+++ src/features/cards-list/CardsList.tsx
@@ -1,22 +1,14 @@
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { useAppSelector } from '../../app/hooks';
 import { Card } from '../card/Card';
 import { cardsInSet } from './cardsListSlice';
 import { selectBoard } from '../board/boardSlice';
 
 import style from './CardsList.module.css';
 import { CardCreationForm } from '../card-creation-form/CardCreationForm';
-import { activeCreationForm, selectCardCreationForm } from '../card-creation-form/cardCreationFormSlice';
-import { CreateButton } from '../create-button/CreateButton';
 
 export function CardsList() {
   const openSetID = useAppSelector(selectBoard);
   const cards = useAppSelector(cardsInSet(openSetID));
-  const dispatch = useAppDispatch();
-  const activityForm = useAppSelector(selectCardCreationForm)
-
-  function handleClick() {
-    dispatch(activeCreationForm(true));
-  }
 
 	return (
     <div>
@@ -24,11 +16,7 @@ export function CardsList() {
         {cards.map(card => (
           <Card key={card.id} card={card} />
         ))}
-        {
-          activityForm.activityCreationForm ?
-          <CardCreationForm /> :
-          <CreateButton onClick={handleClick} />
-        }
+        <CardCreationForm />
       </div>
     </div>
 	)
--- src/features/sets-list/SetsList.tsx
+++ src/features/sets-list/SetsList.tsx
@@ -1,30 +1,18 @@
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { useAppSelector } from '../../app/hooks';
 import styles from './SetsList.module.css';
 import { Set } from '../set/Set';
 import { allSets } from './setsListSlice';
 import { SetCreationForm } from '../set-creation-form/SetCreationForm';
-import { activeCreationForm, selectSetCreationForm } from '../set-creation-form/setCreationFormSlice';
-import { CreateButton } from '../create-button/CreateButton';
 
 export function SetsList() {
-  const dispatch = useAppDispatch();
   const sets = useAppSelector(allSets);
-  const activityForm = useAppSelector(selectSetCreationForm)
-
-  function handleClick() {
-    dispatch(activeCreationForm(true))
-  }
 
 	return (
     <div className={styles.setsList}>
       {sets.map(set => (
         <Set key={set.id} set={set} />
       ))}
-      {
-        activityForm.activityCreationForm ?
-        <SetCreationForm /> :
-        <CreateButton onClick={handleClick} />
-      }
+        <SetCreationForm />
     </div>
 	)
 }
